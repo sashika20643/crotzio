18e3f002a08a33842ab6562b4fabbeafe2c184d0
sashika20643 <sashikadulaj20643@gmail.com>
2023-01-31 23:47:11 +0530

    product more detail popup added

--- resources/views/common/product.blade.php
+++ resources/views/common/product.blade.php
@@ -27,8 +27,8 @@
              <div class="box">
                 <div class="option_container">
                    <div class="options">
-                      <a href="" class="option1">
-                        {{$product->catagory}}
+                      <a class="option1" data-toggle="modal" data-target="#model{{$product->id}}">
+                        Show details
                       </a>
                       <a href="" class="option2">
                       Buy Now
@@ -54,6 +54,44 @@
                 </div>
              </div>
           </div>
+
+
+
+
+  <!-- Modal -->
+  <div class="modal fade" id="model{{$product->id}}" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
+    <div class="modal-dialog modal-dialog-centered" role="document">
+      <div class="modal-content">
+        <div class="modal-header">
+          <h5 class="modal-title" id="exampleModalLongTitle"> {{$product->title}}</h5>
+          <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+            <span aria-hidden="true">&times;</span>
+          </button>
+        </div>
+        <div class="modal-body d-flex">
+            <div class="img-box " >
+                <img src="{{asset('product/'.$product->image)}}" alt="" style="max-width:200px !important">
+             </div>
+             <div class="container">
+
+                <p>
+                    {{$product->description}}
+                </p>
+                <h6>Catagory: <span style="color:orange"> {{$product->catagory}} </span></h6>
+
+                <h6>original price: <span style="color: blue"> {{$product->price}}/= </span></h6>
+                <h6>Discount price: <span style="color: green"> {{$product->discount_price}}/= </span></h6>
+             </div>
+
+        </div>
+        <div class="modal-footer">
+          <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+          <button type="button" class="btn btn-primary">Add to cart</button>
+        </div>
+      </div>
+    </div>
+  </div>
+
 
 
           @endforeach
