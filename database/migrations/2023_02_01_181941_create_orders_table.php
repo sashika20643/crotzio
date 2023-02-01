2293858916575a54529787e41ddd4cc951a7a0bb
sashika20643 <sashikadulaj20643@gmail.com>
2023-02-02 01:22:10 +0530

    ProductOrder and Order table created

--- database/migrations/2023_02_01_181941_create_orders_table.php
+++ database/migrations/2023_02_01_181941_create_orders_table.php
@@ -15,6 +15,19 @@ return new class extends Migration
     {
         Schema::create('orders', function (Blueprint $table) {
             $table->id();
+            $table->string('u_name')->nullable();
+            $table->string('email')->nullable();
+            $table->string('phone')->nullable();
+            $table->string('adress')->nullable();
+            $table->string('u_id')->nullable();
+            $table->string('postal_code')->nullable();
+
+            $table->string('paymet_status')->nullable();
+            $table->string('deliver_status')->nullable();
+
+
+
+
             $table->timestamps();
         });
     }
